1c97ab94632a1323ff58493333c9b61aae1b118d
Brandon Nolan <brandon.nolan@stratahealth.com>
2019-11-18 16:50:42 -0700

    Started working on oAuth2 client config, running into some issues. I'll work on them more at home

--- src/main/java/com/oAuthLearning/oauth/sso/learning/Application.java
+++ src/main/java/com/oAuthLearning/oauth/sso/learning/Application.java
@@ -1,13 +1,83 @@
 package com.oAuthLearning.oauth.sso.learning;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Qualifier;
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.security.oauth2.client.EnableOAuth2Sso;
+import org.springframework.boot.autoconfigure.security.oauth2.resource.ResourceServerProperties;
+import org.springframework.boot.autoconfigure.security.oauth2.resource.UserInfoTokenServices;
+import org.springframework.boot.context.properties.ConfigurationProperties;
+import org.springframework.boot.web.embedded.tomcat.TomcatServletWebServerFactory;
+import org.springframework.boot.web.servlet.server.ServletWebServerFactory;
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.annotation.Bean;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.oauth2.client.OAuth2ClientContext;
+import org.springframework.security.oauth2.client.OAuth2RestTemplate;
+import org.springframework.security.oauth2.client.filter.OAuth2ClientAuthenticationProcessingFilter;
+import org.springframework.security.oauth2.client.token.grant.code.AuthorizationCodeResourceDetails;
+import org.springframework.security.web.authentication.www.BasicAuthenticationFilter;
+import org.springframework.web.bind.annotation.RestController;
+
+import javax.servlet.Filter;
+import java.util.Arrays;
 
 @SpringBootApplication
-public class Application {
+@EnableOAuth2Sso
+@RestController
+public class Application {//extends WebSecurityConfigurerAdapter {
+//
+//	@Qualifier("mine")
+//	@Autowired private OAuth2ClientContext oauth2ClientContext;
 
 	public static void main(String[] args) {
 		SpringApplication.run(Application.class, args);
 	}
+
+//	@Override
+//	protected void configure(HttpSecurity http) throws Exception {
+//		http.antMatcher("/**").authorizeRequests()
+//      .and().addFilterBefore(ssoFilter(), BasicAuthenticationFilter.class);
+//	}
+//
+//	private Filter ssoFilter() {
+//		OAuth2ClientAuthenticationProcessingFilter facebookFilter = new OAuth2ClientAuthenticationProcessingFilter("/login/facebook");
+//		OAuth2RestTemplate facebookTemplate = new OAuth2RestTemplate(facebook(), oauth2ClientContext);
+//		facebookFilter.setRestTemplate(facebookTemplate);
+//		UserInfoTokenServices tokenServices = new UserInfoTokenServices(facebookResource().getUserInfoUri(), facebook().getClientId());
+//		tokenServices.setRestTemplate(facebookTemplate);
+//		facebookFilter.setTokenServices(tokenServices);
+//		return facebookFilter;
+//	}
+//
+//	@Bean
+//	@ConfigurationProperties("facebook.client")
+//	public AuthorizationCodeResourceDetails facebook() {
+//		return new AuthorizationCodeResourceDetails();
+//	}
+//
+//	@Bean
+//	@ConfigurationProperties("facebook.resource")
+//	public ResourceServerProperties facebookResource() {
+//		return new ResourceServerProperties();
+//	}
+
+//    @Bean
+//    public CommandLineRunner commandLineRunner(ApplicationContext ctx) {
+//        return args -> {
+//
+//            System.out.println("Let's inspect the beans provided by Spring Boot:");
+//
+//            String[] beanNames = ctx.getBeanDefinitionNames();
+//            Arrays.sort(beanNames);
+//            for (String beanName : beanNames) {
+//                System.out.println(beanName);
+//            }
+//
+//        };
+//    }
 
 }
